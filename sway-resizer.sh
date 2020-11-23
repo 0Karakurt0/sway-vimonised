07c94ebb5de288fd8f4c034406cce7396107317c
0Karakurt0 <karakurtspider.s0net@gmail.com>
2020-11-23 17:12:32 +0200

    Reworked resize logic, now it works.

--- sway-resizer.sh
+++ sway-resizer.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-string=$1
-length=${#string}
-number=${string:0:$(($length -1))}
-direction=${string:(-1)}
-case $direction in
-    h) swaymsg resize shrink width $number ;;
-    j) swaymsg resize shrink height $number ;;
-    k) swaymsg resize grow height $number ;;
-    l) swaymsg resize grow width $number ;;
+[ -f $2 ] || touch $2
+number=$(cat $2);
+case $1 in
+    [0-9]) echo "$number$1" > $2 ;;
+    left) swaymsg resize shrink width $number;;
+    up) swaymsg resize shrink height $number;;
+    down) swaymsg resize grow height $number;;
+    right) swaymsg resize grow width $number;;
+    clear)  echo "0" > $2;;
+    *) echo "Wrong input!";;
 esac
 # TODO
 # Make it sh
